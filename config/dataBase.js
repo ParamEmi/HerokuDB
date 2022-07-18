7724d0365883d72f3e71c798f2c9d261df8be7d4
ParamEmi <paramjeet.eminenece@gmail.com>
2022-07-18 14:53:19 +0530

    push code to heroku

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,5 +1,12 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/userApp");
+const { mongoose }  = require("mongoose");
+const db = 'mongodb+srv://param:aQawvPWLUlgtSKoe@cluster0.gffac.mongodb.net/appDB?retryWrites=true&w=majority';
+//mongoose.connect("mongodb://localhost:27017/userApp")
+mongoose.Promise = global.Promise;
+mongoose.connect(db, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+mongoose.set('debug', true);
 var dataBase = mongoose.connection;
 
 dataBase.on("connected", function () {
